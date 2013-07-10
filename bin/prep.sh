bd52ce733ff5c2353f0544a1839474ba8c96bc56
Sungjin Han <meinside@gmail.com>
2013-07-10 19:52:29 +0900

    Update prep.sh

--- bin/prep.sh
+++ bin/prep.sh
@@ -5,7 +5,7 @@
 # for setting up various things automatically in OSX
 # (https://raw.github.com/meinside/osxconfigs/master/bin/prep.sh)
 # 
-# last update: 2013.04.03.
+# last update: 2013.07.10.
 # 
 # by meinside@gmail.com
 
@@ -15,17 +15,6 @@ TMP_DIR="$HOME/configs.tmp"
 
 echo -e "\033[32mThis script will setup various things for OSX\033[0m\n"
 
-# install brew
-if ! which brew > /dev/null; then
-	echo -e "\033[33m>>> installing brew... (run this script again after installation)\033[0m\n"
-	ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-	echo
-	echo -e "\033[31m>>> make sure '/usr/local/bin' is on the top of /etc/paths file.\033[0m"
-	echo "$ sudo vi /etc/paths"
-	echo
-	exit 0
-fi
-
 # clone config files
 echo -e "\033[33m>>> cloning config files...\033[0m\n"
 rm -rf $TMP_DIR
@@ -34,10 +23,10 @@ shopt -s dotglob nullglob
 mv $TMP_DIR/* $HOME/
 rm -rf $TMP_DIR
 
-# install RVM for single user
+# install Homebrew, RVM and Ruby for single user
 echo
-echo -e "\033[33m>>> installing RVM...\033[0m\n"
-curl -L https://get.rvm.io | bash -s stable --autolibs=enable
+echo -e "\033[33m>>> installing Homebrew, RVM and Ruby...\033[0m\n"
+curl -L https://get.rvm.io | bash -s stable --autolibs=homebrew --ruby
 
 # re-login for loading configs
 echo
