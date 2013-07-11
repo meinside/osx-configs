53865920d8b0cf763a8ae14fca4997dc291a3ad0
Sungjin Han <meinside@gmail.com>
2013-07-11 11:37:49 +0900

    edit bin/prep.sh

--- bin/prep.sh
+++ bin/prep.sh
@@ -5,7 +5,7 @@
 # for setting up various things automatically in OSX
 # (https://raw.github.com/meinside/osxconfigs/master/bin/prep.sh)
 # 
-# last update: 2013.07.10.
+# last update: 2013.07.11.
 # 
 # by meinside@gmail.com
 
@@ -13,22 +13,24 @@
 REPOSITORY="git://github.com/meinside/osxconfigs.git"
 TMP_DIR="$HOME/configs.tmp"
 
-echo -e "\033[32mThis script will setup various things for OSX\033[0m\n"
+echo -e "\033[32mThis script will setup various things for OSX\033[0m"
 
 # clone config files
-echo -e "\033[33m>>> cloning config files...\033[0m\n"
+echo -e "\033[33m>>> cloning config files...\033[0m"
 rm -rf $TMP_DIR
 git clone $REPOSITORY $TMP_DIR
 shopt -s dotglob nullglob
 mv $TMP_DIR/* $HOME/
 rm -rf $TMP_DIR
 
-# install Homebrew, RVM and Ruby for single user
-echo
-echo -e "\033[33m>>> installing Homebrew, RVM and Ruby...\033[0m\n"
+# install Homebrew
+echo -e "\033[33m>>> installing Homebrew...\033[0m"
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+# install RVM and Ruby for single user
+echo -e "\033[33m>>> installing RVM and Ruby...\033[0m"
 curl -L https://get.rvm.io | bash -s stable --autolibs=homebrew --ruby
 
 # re-login for loading configs
 echo
 echo -e "\033[31m*** logout, and login again for reloading configs ***\033[0m"
-echo
