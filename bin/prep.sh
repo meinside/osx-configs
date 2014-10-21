a38f01eec0c65371da6338804d5577063dad0894
Sungjin Han <meinside@gmail.com>
2014-10-21 10:05:45 +0900

    edit bin/prep.sh

--- bin/prep.sh
+++ bin/prep.sh
@@ -5,7 +5,7 @@
 # for setting up various things automatically in OSX
 # (https://raw.github.com/meinside/osx-configs/master/bin/prep.sh)
 # 
-# last update: 2014.06.30.
+# last update: 2014.10.21.
 # 
 # by meinside@gmail.com
 
@@ -25,7 +25,7 @@ rm -rf $TMP_DIR
 
 # install Homebrew
 echo -e "\033[33m>>> installing Homebrew...\033[0m"
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install RVM and Ruby for single user
 echo -e "\033[33m>>> installing RVM and Ruby...\033[0m"
